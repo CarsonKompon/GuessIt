dd6a2498e30918f767d8497a40e55a4371a10f29
Carson Kompon <carsokompo@gmail.com>
2023-08-26 10:34:11 -0400

    Fix when sounds are played and when networked conditions play out

--- code/UI/GameMenu/GameMenu.cs
+++ code/UI/GameMenu/GameMenu.cs
@@ -4,7 +4,6 @@ using Sandbox.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 
 namespace GuessIt;
 
@@ -14,20 +13,6 @@ enum LOBBY_STATE
     CHOOSING_WORD,
     PLAYING,
     RESULTS
-}
-
-enum LOBBY_MESSAGE
-{
-    NONE,
-    START_ROUND,
-    CHOOSE_WORD,
-    DRAW,
-    REQUEST_CANVAS,
-    SEND_CANVAS,
-    CORRECT_GUESS,
-    REVEAL_WORD,
-    SHOW_RESULTS,
-    END_GAME
 }
 
 public partial class GameMenu
@@ -86,6 +71,7 @@ public partial class GameMenu
                 entry.Lobby = Lobby;
                 entry.SetScore(GetPlayerScore(friend.Id));
             }
+            UpdatePlayerOrder();
         }
     }
 
@@ -269,6 +255,17 @@ public partial class GameMenu
 
         CreateChatEntry("The word was: " + Guess, "");
 
+        if(Drawing.Id == Game.SteamId)
+        {
+            if(CorrectPlayers.Count > 0)
+            {
+                Audio.Play("ui.reveal.correct");
+            }
+            else
+            {
+                Audio.Play("ui.reveal.wrong");
+            }
+        }
         if(CorrectPlayers.Contains(new Friend(Game.SteamId)))
         {
             Audio.Play("ui.reveal.correct");
@@ -452,9 +449,15 @@ public partial class GameMenu
             }
         }
         entries.Sort((a, b) => (int)(GetPlayerScore(b.Player.Id) - GetPlayerScore(a.Player.Id)));
+        int rank = 1;
+        long previousScore = 0;
         for(int i=0; i<entries.Count; i++)
         {
+            long score = GetPlayerScore(entries[i].Player.Id);
+            if(i == 0) previousScore = score;
+            if(score != previousScore) rank++;
             PlayerList.SetChildIndex(entries[i], i);
+            entries[i].SetRank(rank);
         }
     }
 
@@ -487,6 +490,7 @@ public partial class GameMenu
         entry.Player = friend;
         entry.Lobby = Lobby;
         entry.SetScore(GetPlayerScore(friend.Id));
+        UpdatePlayerOrder();
     }
 
     void OnMemberLeave(Friend friend)
@@ -663,277 +667,5 @@ public partial class GameMenu
     protected override int BuildHash()
     {
         return HashCode.Combine(LogoClass(), Lobby.MemberCount, MathF.Floor(GameTimer));
-    }
-
-
-    // NETWORKING SHIT
-
-    void OnNetworkMessage(ILobby.NetworkMessage msg)
-    {
-        ByteStream data = msg.Data;
-
-        Log.Info(msg.Source);
-
-        ushort messageId = data.Read<ushort>();
-
-        Log.Info((LOBBY_MESSAGE)messageId);
-
-        switch((LOBBY_MESSAGE)messageId)
-        {
-            case LOBBY_MESSAGE.START_ROUND:
-                long drawingId = data.Read<long>();
-                Drawing = new Friend(drawingId);
-                StartingPlayers = ListFromString(Lobby.Data["players"]);
-                if(Lobby.Data.ContainsKey("played")) FinishedPlayers = ListFromString(Lobby.Data["played"]);
-                else FinishedPlayers?.Clear();
-                StartRound();
-                break;
-            
-            case LOBBY_MESSAGE.CHOOSE_WORD:
-                int byteLength = data.Read<int>();
-                byte[] guessBytes = new byte[byteLength];
-                for(int i=0; i<byteLength; i++)
-                {
-                    guessBytes[i] = data.Read<byte>();
-                }
-                Guess = System.Text.Encoding.Unicode.GetString(guessBytes);
-                if(Lobby.Owner.Id == Game.SteamId)
-                {
-                    Lobby.SetData("guess", Guess);
-                }
-                GameTimer = 120f;
-                StartDrawing();
-                break;
-
-            case LOBBY_MESSAGE.DRAW:
-                Color32 color = data.Read<Color32>();
-                int size = data.Read<ushort>();
-                ushort pointCount = data.Read<ushort>();
-                List<Vector2> points = new List<Vector2>();
-                for(int i=0; i<pointCount; i++)
-                {
-                    ushort x = data.Read<ushort>();
-                    ushort y = data.Read<ushort>();
-                    points.Add(new Vector2(x, y));
-                }
-                Draw(points, color, size);
-                break;
-            
-            case LOBBY_MESSAGE.REQUEST_CANVAS:
-                if(Lobby.Owner.Id == Game.SteamId && LobbyState == LOBBY_STATE.PLAYING)
-                {
-                    long friendId = data.Read<long>();
-                    Friend friend = new Friend(friendId);
-                    if(!Lobby.Members.Contains(friend)) break;
-                    NetworkSendCanvas(friend);
-                }
-                break;
-            
-            case LOBBY_MESSAGE.SEND_CANVAS:
-                if(LobbyState != LOBBY_STATE.PLAYING) break;
-                if(Canvas is not Texture)
-                {
-                    ResetCanvas();
-                }
-
-                int pixelCount = data.Read<int>();
-                Color32[] pixels = new Color32[pixelCount];
-                for(int i=0; i<pixelCount; i++)
-                {
-                    pixels[i] = data.Read<Color32>();
-                }
-                Canvas.Update(pixels);
-                CanvasPanel.SetTexture(Canvas);
-                StateHasChanged();
-
-                Drawing = new Friend(long.Parse(Lobby.Data["drawing"]));
-                StartingPlayers = ListFromString(Lobby.Data["players"]);
-                if(Lobby.Data.ContainsKey("played")) FinishedPlayers = ListFromString(Lobby.Data["played"]);
-                else FinishedPlayers?.Clear();
-                Guess = Lobby.Data["guess"];
-                StartDrawing();
-                break;
-
-            case LOBBY_MESSAGE.CORRECT_GUESS:
-                if(LobbyState != LOBBY_STATE.PLAYING) break;
-
-                long playerId = data.Read<long>();
-                Friend player = new Friend(playerId);
-                long playerScore = data.Read<long>();
-                long drawerId = data.Read<long>();
-                Friend drawing = new Friend(drawerId);
-                long drawingScore = data.Read<long>();
-
-                CreateChatEntry(player.Name, " guessed correctly!");
-                Audio.Play("ui.guess.correct");
-
-                GivePlayerScore(player.Id, playerScore, false);
-                GivePlayerScore(drawing.Id, drawingScore, false);
-                UpdatePlayerOrder();
-
-                float timer = GameTimer;
-                if(Lobby.Data.ContainsKey("timer")) timer = float.Parse(Lobby.Data["timer"]);
-                if(timer < GameTimer) GameTimer = timer;
-
-                if(Lobby.Owner.Id != Game.SteamId)
-                {
-                    if(Lobby.Data.ContainsKey("timer")) GameTimer = float.Parse(Lobby.Data["timer"]);
-                }
-
-                break;
-
-            case LOBBY_MESSAGE.REVEAL_WORD:
-                if(LobbyState != LOBBY_STATE.PLAYING) break;
-
-                byteLength = data.Read<int>();
-                byte[] wordBytes = new byte[byteLength];
-                for(int i=0; i<byteLength; i++)
-                {
-                    wordBytes[i] = data.Read<byte>();
-                }
-                if(Lobby.Data.ContainsKey("correct"))
-                {
-                    CorrectPlayers = ListFromString(Lobby.Data["correct"]);
-                }
-                else
-                {
-                    CorrectPlayers.Clear();
-                }
-                Guess = System.Text.Encoding.Unicode.GetString(wordBytes);
-                RevealAnswer();
-                break;
-            
-            case LOBBY_MESSAGE.SHOW_RESULTS:
-                ShowResults();
-                break;
-            
-            case LOBBY_MESSAGE.END_GAME:
-                EndGame();
-                break;
-        }
-    }
-
-    void NetworkStartRound()
-    {
-        Log.Info("Starting on the network");
-        ByteStream data = ByteStream.Create(10);
-        data.Write((ushort)LOBBY_MESSAGE.START_ROUND);
-        data.Write(Drawing.Id);
-
-        Lobby.BroadcastMessage(data);
-    }
-
-    void NetworkChooseWord()
-    {
-        byte[] guessBytes = System.Text.Encoding.Unicode.GetBytes(Guess);
-
-        ByteStream data = ByteStream.Create(6 + guessBytes.Length);
-        data.Write((ushort)LOBBY_MESSAGE.CHOOSE_WORD);
-        data.Write((int)guessBytes.Length);
-        for(int i=0; i<guessBytes.Length; i++)
-        {
-            data.Write(guessBytes[i]);
-        }
-
-        Lobby.BroadcastMessage(data);
-    }
-
-    public void NetworkDraw(List<Vector2> points, Color color, int size)
-    {
-        ByteStream data = ByteStream.Create(10 + (2 * points.Count));
-        data.Write((ushort)LOBBY_MESSAGE.DRAW);
-        data.Write(color.ToColor32());
-        data.Write((ushort)size);
-        data.Write((ushort)points.Count);
-        for(int i=0; i<points.Count; i++)
-        {
-            data.Write((ushort)points[i].x);
-            data.Write((ushort)points[i].y);
-        }
-
-        Lobby.BroadcastMessage(data);
-    }
-
-    void NetworkRequestCanvas()
-    {
-        Header.SetOverride("Loading...");
-        ByteStream data = ByteStream.Create(10);
-        data.Write((ushort)LOBBY_MESSAGE.REQUEST_CANVAS);
-        data.Write((long)Game.SteamId);
-
-        Lobby.OwnerMessage(data);
-    }
-
-    void NetworkSendCanvas(Friend friend)
-    {
-        if(LobbyState != LOBBY_STATE.PLAYING) return;
-        if(Lobby.Owner.Id != Game.SteamId) return;
-        if(Canvas is not Texture) return;
-
-        Color32[] pixels = Canvas.GetPixels();
-
-        ByteStream data = ByteStream.Create(6 + (pixels.Length * 4));
-        data.Write((ushort)LOBBY_MESSAGE.SEND_CANVAS);
-        data.Write((int)pixels.Length);
-        for(int i=0; i<pixels.Length; i++)
-        {
-            data.Write(pixels[i]);
-        }
-
-        Lobby.SendMessage(friend, data);
-    }
-
-    void NetworkCorrectGuess(Friend friend, long playerScore, long drawingScore)
-    {
-        if(Lobby.Owner.Id != Game.SteamId) return;
-        if(LobbyState != LOBBY_STATE.PLAYING) return;
-
-        ByteStream data = ByteStream.Create(34);
-        data.Write((ushort)LOBBY_MESSAGE.CORRECT_GUESS);
-        data.Write((long)friend.Id);
-        data.Write((long)playerScore);
-        data.Write((long)Drawing.Id);
-        data.Write((long)drawingScore);
-
-        Lobby.BroadcastMessage(data);
-    }
-
-    void NetworkRevealAnswer()
-    {
-        if(Lobby.Owner.Id != Game.SteamId) return;
-        if(LobbyState != LOBBY_STATE.PLAYING) return;
-        GameTimer = 0f;
-
-        byte[] wordBytes = System.Text.Encoding.Unicode.GetBytes(Guess);
-        ByteStream data = ByteStream.Create(6 + wordBytes.Length);
-        data.Write((ushort)LOBBY_MESSAGE.REVEAL_WORD);
-        data.Write((int)wordBytes.Length);
-        for(int i=0; i<wordBytes.Length; i++)
-        {
-            data.Write(wordBytes[i]);
-        }
-
-        Lobby.BroadcastMessage(data);
-    }
-
-    void NetworkShowResults()
-    {
-        if(Lobby.Owner.Id != Game.SteamId) return;
-        GameTimer = 10f;
-
-        ByteStream data = ByteStream.Create(2);
-        data.Write((ushort)LOBBY_MESSAGE.SHOW_RESULTS);
-
-        Lobby.BroadcastMessage(data);
-    }
-
-    void NetworkEndGame()
-    {
-        if(Lobby.Owner.Id != Game.SteamId) return;
-
-        ByteStream data = ByteStream.Create(2);
-        data.Write((ushort)LOBBY_MESSAGE.END_GAME);
-
-        Lobby.BroadcastMessage(data);
     }
 }
